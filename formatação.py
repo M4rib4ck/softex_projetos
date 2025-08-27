d7df24d28a4d76d07d4bf70e65070d6ad4a2f89a
M4rib4ck <223533892+M4rib4ck@users.noreply.github.com>
2025-08-27 15:32:38 -0300

    melhorando o código

--- formatação.py
+++ formatação.py
@@ -5,27 +5,27 @@ verificar se tem 11 caracteres e se todos são números
 se o número for válido, formatar ao padrão (XX)XXXXX-XXXX
 imprimir número ou mensaem de erro
 '''
-
-num = input("Digite o número com 11 digitos:" )
-if not num.isdigit():
-    print("Número inválido. Por favor digite apenas números.")
-elif len(num) != 11:
-    print("Número inválido! Deve conter 11 dígitos!")
-else:
-    valido = True
-    for c in num:
-        contador_repetido = 0
-        for d in num:
-            if c==d:
-                contador_repetido +=1
-                if contador_repetido >=3:   
-                    valido = False
-                    break
-if not valido:
-    print("Numero inválido por haver repetições")
-else:
-    ddd = num[:2]
-    primeira = num[2:7]
-    segunda = num[:7]
-    formatado = f"({ddd}){primeira}-{segunda}"
-    print(f"Número válido!! {formatado}")
+while True:
+    num = input("Digite o número com 11 digitos:" )
+    valido = False
+    if not num.isdigit():
+        print("Número inválido. Por favor digite apenas números.")
+    elif len(num) != 11:
+        print("Número inválido! Deve conter 11 dígitos!")
+    else:
+        valido = True
+        for c in num:
+            contador_repetido = 0
+            for d in num:
+                if c==d:
+                    contador_repetido +=1
+            if contador_repetido >=3:   
+                valido = False
+                print("Número inválido por haver repetições")
+                break
+    if valido:
+        ddd = num[:2]
+        primeira = num[2:7]
+        segunda = num[7:]
+        formatado = (f"({ddd}){primeira}-{segunda}")
+        print(f"Número válido!! {formatado}")
