cb3aa4ac8fafe8cfb3180ecca21b769dfd400f13
Maria Eduarda <mariaeduarda06.cs@gmail.com>
2025-08-29 14:26:18 -0300

    Desafio do triangulo finalizado

--- desafio_triangulo.py
+++ desafio_triangulo.py
@@ -9,9 +9,9 @@ As Regras do Jogo
     ///
 '''
 lados = []
-for i in range(1,4):
+for i in range(1, 4):
     while True:
-        lado = input(f"Digite o valor do lado {i}º lado do triângulo: ")
+        lado = input(f"Digite o valor do {i}º lado do triângulo: ")
         if lado.isdigit():
             lado = int(lado)
             if lado > 0:
@@ -19,7 +19,9 @@ for i in range(1,4):
                 break
             else:
                 print("Lado inválido! Deve ser um número positivo.")
-
+        else:
+            print("Lado inválido! Deve ser um número.")
+            
 l1, l2, l3 = lados
 if (l1 < l2 + l3 and l1 > abs(l2 - l3) and
     l2 < l1 + l3 and l2 > abs(l1 - l3) and
