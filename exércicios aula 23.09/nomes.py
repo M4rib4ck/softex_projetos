b6b9bf11565b3c1bdaa24d61102a41f62ce8aabc
M4rib4ck <223533892+M4rib4ck@users.noreply.github.com>
2025-09-22 15:08:50 -0300

    exercicios feitos em aula

---
+++ exércicios aula 23.09/nomes.py
@@ -0,0 +1,8 @@
+
+class Pessoas:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def __str__(self):
+        return f"{self.nome}, {self.idade} anos"
