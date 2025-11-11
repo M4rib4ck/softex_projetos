462b09d2fce3026ef8e4abc402877c3ea54cc22d
Maria Eduarda <223533892+M4rib4ck@users.noreply.github.com>
2025-11-11 15:35:39 -0300

    add: ajuste na view home e templates

--- modulo_04/core/views.py
+++ modulo_04/core/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse 
+from .models import Tarefa
 
 # Create your views here.
 def home(request):
+    todas_as_tarefas = Tarefa.objects.all()
+    
     context = {
-        'nome_usuario': 'Mari',
-        'tecnologias': ['python', 'Django', 'HRML', 'CSS']
-    }
+        'nome_usuario': 'Júnior',
+        'tecnologias': ['python', 'Django', 'Models', 'Admin'],
+        'tarefas': todas_as_tarefas
+        }
+
     # return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
     return render(request, 'home.html', context)
 
